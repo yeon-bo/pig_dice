2ca7ab2e7b884e0d9a9f289343a0d0d5443e886d
hyejic <hyeji0314@gmail.com>
2021-11-25 19:50:49 +0900

    feat: if eles conditional

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // random
-const randomNum = Math.floor(Math.random() * 7)
+const randomNum = Math.floor(Math.random() * 6) + 1
 console.log(randomNum)
+
+//if
+let total;
+if(randomNum !== 1){
+  total += randomNum;
+  console.log('total',total)
+}else{
+  total = 0;
+  console.log('total',total)
+  alert('다음 턴')
+}
