1be37436bf20c4d9bd518b2d5f644f79fcc98e1e
yeon-bo <dylk97@gmail.com>
2021-11-25 20:52:39 +0900

    feat: Add for 1=0

---
+++ main.js
@@ -0,0 +1,12 @@
+let rand = Math.floor(Math.random()*6) + 1;
+const mySum = [];
+
+const a = rand == 1 ? "0" : "숫자더하기" ;
+
+// function diceAdd (rand) {
+//     mySum.push(rand);
+//     const sum = mySum.reduce((a,b) => (a+b));
+//     return sum;
+// }
+
+console.log(a)
