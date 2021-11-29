2774f7e1ed19ce85abbae96cfdd9d425dc625e40
himchan <himchan9494@gmail.com>
2021-11-30 00:15:42 +0900

    feat: Adding player reset function
    Function for reset all players

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const layerPop = document.querySelector(".l-layerpop");
 const playerInput = document.querySelector(".input");
 const playerUl = document.querySelector(".player-name ul");
 const playerAddBtn = document.querySelector(".player-add");
+const playerResetBtn = document.querySelector(".btn_reset");
 
+// 플레이어 추가 함수
 function addPlayer() {
   // input 태그 내의 플레이어명
   const playerName = playerInput.value;
@@ -14,21 +16,23 @@ function addPlayer() {
   //player_arr 배열에 플레이어 추가
   player_arr.push(playerName);
 
-  // 플레이어 추가를 위한 li 태그 생성
   const playerLi = document.createElement("li");
-
-  // li 태그 내에, 플레이어명 추가
   playerLi.append(playerName);
-
-  // Ul 태그 내에, 플레이어명이 추가된 li 태그 추가
   playerUl.append(playerLi);
 
   // input 태그 리셋
   playerInput.value = "";
 }
 
-// 플레이어 추가 버튼 클릭시, addPlayer 함수 실행
 playerAddBtn.addEventListener("click", addPlayer);
+
+// 플레이어 리셋 함수
+function resetPlayer() {
+  playerUl.innerHTML = "";
+  player_arr = [];
+}
+
+playerResetBtn.addEventListener("click", resetPlayer);
 
 function pigdice() {
   const diceNum = document.querySelector("#diceNum");
